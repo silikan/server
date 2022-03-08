520f8db3f322bf0b858115aa6957ffee3b489496
ismailbentabett <ismailbentabett@gmail.com>
2022-03-08 22:40:21 +0100

    server 1..0.0 : just installed websockets thats all

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => 'eu',
-                'useTLS' => true
-              ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'http'
             ],
         ],
 
