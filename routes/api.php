cff6213385435fda6402db504ada4edf8d3b435e
ismailbentabett <ismailbentabett@gmail.com>
2022-04-07 22:09:42 +0100

    client 1.0.0 : i refixed the chat

--- routes/api.php
+++ routes/api.php
@@ -94,6 +94,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
   Route::post('/users/auth/avatar', [AvatarController::class, 'upload_user_photo']);
 
   Route::post('gig', [GigController::class, 'store']);
+  Route::get('chat/{room_id}', [ChatController::class, 'getChat']);
 
   Route::post('gig/image/{gigId}', [GigImagesController::class, 'store']);
   Route::post('request', [ClientRequestController::class, 'store']);
