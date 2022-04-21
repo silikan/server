0bd054468c5e2c5adc1bab19f7de65f256904609
ismailbentabett <ismailbentabett@gmail.com>
2022-04-22 00:38:42 +0100

    server 1.0.0 :  some changes on tnt search*

--- config/scout.php
+++ config/scout.php
@@ -16,11 +16,11 @@ return [
     */
     'tntsearch' => [
         'storage'  => storage_path(), //place where the index files will be stored
-        'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
+        'fuzziness' => env('TNTSEARCH_FUZZINESS', true),
         'fuzzy' => [
-            'prefix_length' => 2,
+            'prefix_length' => 4,
             'max_expansions' => 50,
-            'distance' => 2
+            'distance' => 4
         ],
         'asYouType' => false,
         'searchBoolean' => env('TNTSEARCH_BOOLEAN', false),
